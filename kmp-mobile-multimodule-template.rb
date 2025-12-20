f212ee3c23712decea287add129fa1da802eb3e6
kk <59042614+kk-house-777@users.noreply.github.com>
2025-12-20 19:48:23 +0900

    Update URL and SHA256 for version 0.0.3

--- kmp-mobile-multimodule-template.rb
+++ kmp-mobile-multimodule-template.rb
@@ -1,9 +1,9 @@
 class KmpMobileMultimoduleTemplate < Formula
   desc "KMP + Tuist multi-module project template generator"
   homepage "https://github.com/kk-house-777/kmp-mobile-tuist-template"
-  url "https://github.com/kk-house-777/kmp-mobile-tuist-template/archive/refs/tags/v0.0.2.tar.gz"
+  url "https://github.com/kk-house-777/kmp-mobile-tuist-template/archive/refs/tags/v0.0.3.tar.gz"
   # 例 : curl -L https://github.com/kk-house-777/kmp-mobile-tuist-template/archive/refs/tags/v0.0.2.tar.gz | shasum -a 256
-  sha256 "f436d5af0aff4ea9d1ce11efd452c68eccadad6dbed8aeaed9200e035b8d9250"
+  sha256 "4098d5de012ee3ca80011304f7132f8d66899a915d996d9371b1772db76cc9b8"
   license "MIT"
 
   depends_on "cookiecutter"
