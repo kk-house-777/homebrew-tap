001bba57620528cec37d4d0a5ad07b50f4f7ef81
kk-house-777 <intelinside.7575@gmail.com>
2026-01-05 18:17:00 +0900

    fix-sha256

--- kotlin-native-template.rb
+++ kotlin-native-template.rb
@@ -1,9 +1,9 @@
 class KotlinNativeTemplate < Formula
   desc "Kotlin/Native template generator"
   homepage "https://github.com/kk-house-777/kotlin-native-template"
-  url "https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/v0.0.3.tar.gz"
-  # 例 : curl -L https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/v0.0.3.tar.gz | shasum -a 256
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/0.0.3.tar.gz"
+  # 例 : curl -L https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/0.0.3.tar.gz | shasum -a 256
+  sha256 "db8aa548ae32b8fecf8b38081f541bfdb9c11fffa45bc078647327ef6bf643d4"
   license "MIT"
 
   def install
