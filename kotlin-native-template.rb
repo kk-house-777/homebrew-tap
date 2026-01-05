e1254e68bf19e0346804f6103e68246962ea2a3d
kk-house-777 <intelinside.7575@gmail.com>
2026-01-05 18:15:12 +0900

    fix-tag

--- kotlin-native-template.rb
+++ kotlin-native-template.rb
@@ -1,8 +1,8 @@
 class KotlinNativeTemplate < Formula
   desc "Kotlin/Native template generator"
   homepage "https://github.com/kk-house-777/kotlin-native-template"
-  url "https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/v0.0.2.tar.gz"
-  # 例 : curl -L https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/v0.0.2.tar.gz | shasum -a 256
+  url "https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/v0.0.3.tar.gz"
+  # 例 : curl -L https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/v0.0.3.tar.gz | shasum -a 256
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
